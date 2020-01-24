e988b968b9391f3f117034d0f9d44641f37e8fd2
WassiW80 <sayedwaseeyat111@gmail.com>
2020-01-24 11:23:56 +0530

    ADDED: Added third computation.

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -4,3 +4,4 @@ read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 result1=`echo "scale=2;$a+$b*$c" | bc`
 result2=`echo "scale=2;$a*$b+$c" | bc`
+result3=`echo "scale=2;$c+$a/$b" | bc`
