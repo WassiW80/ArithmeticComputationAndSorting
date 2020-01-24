8206d2184a374faf0b4264602c6848c514f51922
WassiW80 <sayedwaseeyat111@gmail.com>
2020-01-24 12:59:38 +0530

    ADDED: Dictionary is used to store results.

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
+
+declare -A resultDictionary
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
@@ -6,3 +8,10 @@ result1=`echo "$a+$b*$c" | bc`
 result2=`echo "$a*$b+$c" | bc`
 result3=`echo "scale=2;$c+$a/$b" | bc`
 result4=`echo "$a%$b+$c" | bc`
+
+resultDictionary[result1]=$result1
+resultDictionary[result2]=$result2
+resultDictionary[result3]=$result3
+resultDictionary[result4]=$result4
+
+
